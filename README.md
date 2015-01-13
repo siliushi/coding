3842645ce727ceab463eb8d298bb09d3d74ab41f
ganzw <ganzwen@gmail.com>
2015-01-13 16:48:31 +0800

    Fix modify file encoding format

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # coding
-change file coding
+Modify the file encoding format
+   
+##描述  
+  
+修改文件编码格式，例如：GBK转UTF8   
+====================  
+  
+修改变量   
+->
+root_path : 需要进行转码的文件路径    
+file_type : 需要进行转码的文件格式，比如html文件   
+from_code : 文件的编码   
+to_code   : 文件的目标编码   
+   
+    
+##实例    
+
+git clone https://github.com/baixuexiyang/coding.git    
+
+查看html文件夹下的内容，会出现乱码    
+
+执行node code.js   
+
+再次查看就正常了   
+    
+            
---
+++ code.js
@@ -0,0 +1,66 @@
+/**
+ * 修改文件编码格式，例如：GBK转UTF8
+ * @param {String} [root_path] [需要进行转码的文件路径]
+ * @param {Array}  [file_type] [需要进行转码的文件格式，比如html文件]
+ * @param {String} [from_code] [文件的编码]
+ * @param {String} [to_code]   [文件的目标编码]
+ */
+
+// 引入包
+var fs = require('fs'),
+  iconv = require('iconv-lite');
+
+// 全局变量
+var root_path = './html',
+    file_type = ['html', 'htm'],
+    from_code = 'GBK',
+    to_code   = 'UTF8';
+
+/**
+ * 判断元素是否在数组内
+ * @date   2015-01-13
+ * @param  {[String]}   elem [被查找的元素]
+ * @return {[bool]}        [description]
+ */
+Array.prototype.inarray = function(elem) {
+  "use strict";
+  var l = this.length;
+  while (l--) {
+    if (this[l] === elem) {
+      return true;
+    }
+  }
+  return false;
+};
+
+
+/**
+ * 转码函数
+ * @date   2015-01-13
+ * @param  {[String]}   root [编码文件目录]
+ * @return {[type]}        [description]
+ */
+function encodeFiles(root) {
+  "use strict";
+  var files = fs.readdirSync(root);
+  files.forEach(function(file) {
+    var pathname = root + '/' + file,
+      stat = fs.lstatSync(pathname);
+    if (!stat.isDirectory()) {
+      var name = file.toString();
+      if (!file_type.inarray(name.substring(name.lastIndexOf('.') + 1))) {
+        return;
+      }
+      fs.writeFile(pathname, iconv.decode(fs.readFileSync(pathname), from_code), {
+        encoding: to_code
+      }, function(err) {
+        if (err) {
+          throw err;
+        }
+      });
+    } else {
+      encodeFiles(pathname);
+    }
+  });
+}
+encodeFiles(root_path);
